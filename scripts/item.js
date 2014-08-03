5ae8faf993e03f2862edd1cc15a82799eb120dab
Matti Schneider <hi@mattischneider.fr>
2014-08-03 23:17:07 +0200

    Initial import.

---
+++ scripts/item.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+	var under	= document.getElementById('under'),
+		over	= document.getElementById('over'),
+		above	= document.getElementById('above')
+		spacer	= document.getElementById('spacer');
+
+	spacer.style.height = window.innerHeight + 'px';
+
+	under.style.position = 'fixed';
+
+	window.addEventListener('scroll', function() {
+		under.style.top = ((above.clientHeight - window.scrollY) / 1.5) + 'px';
+		over.style.top = ((above.clientHeight - window.scrollY) / 1.25) + 'px';
+	});
+
+	window.scrollTo(0, spacer.offsetTop);
+}
